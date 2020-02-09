17c691f4211e154e4c2c4dd290a9e0c2aee5a546
chenzhiy <419083145@qq.com>
2020-02-09 17:53:32 +0800

    更新配置文件

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '26pjh6np9fk#bgip6vx^)@97p^q##vleobl)ms#2lj3@b5p6a_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [‘*’]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
